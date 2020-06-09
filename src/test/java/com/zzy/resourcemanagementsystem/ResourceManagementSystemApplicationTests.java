6125d3991b6b1614baf5a0d866918bacc90bb98f
Ziyao Zeng <>
2020-06-09 18:53:47 +0800

    initial commit

---
+++ src/test/java/com/zzy/resourcemanagementsystem/ResourceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.resourcemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
